d60974fa6a582edb773d9fb9e1d14240a8b20cd2
SavEe2429 <nongsafgml@gmail.com>
2026-02-20 10:02:43 +0700

    update

--- backend/tts.py
+++ backend/tts.py
@@ -5,13 +5,14 @@ def speak_number(num):
     _engine = pyttsx3.init()
     voices = _engine.getProperty('voices')
     # ตั้งค่าเสียง (ปรับได้)
-    _engine.setProperty("rate", 160)   # ความเร็วพูด
+    _engine.setProperty("rate", 200)   # ความเร็วพูด
     _engine.setProperty("volume", 1.0) # ความดัง
 
     """
     รับเลข 1-4 แล้วพูดออกลำโพง
     """
     text_map = {
+        0: "orangjuie",
         1: "one",
         2: "two",
         3: "three",
